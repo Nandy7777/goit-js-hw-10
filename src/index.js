23e24e1aa02153ee57d8bee8d5502bc52b3eb2ac
Nandy7777 <nandy7777@ukr.net>
2022-09-03 00:59:59 +0300

    початок

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import './css/styles.css';
+import countryCard from './templates/country_card.hbs';
+
+const refs = {
+    cardContainer: document.querySelector(`.country-info`)
+}
 
 const DEBOUNCE_DELAY = 300;
+
+fetchCountries().then(rendercountryCard).catch(error => console.log(error))
+
+function fetchCountries(name) {
+    return fetch('https://restcountries.com/v3.1/name/${name}')
+        .then(response => {
+            return response.json();
+        });
+};
+
+
+
+
+function rendercountryCard(country) {
+    const markup = countryCard(country);
+    refs.cardContainer.innerHTML = markup;
+}
